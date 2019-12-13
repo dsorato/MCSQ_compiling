75d5dbf81921d961095f165d21d848ab6ad7955e
dsorato <danielly.sorato@gmail.com>
2019-12-13 13:56:39 +0100

    added modules

--- evs_xml_data_extraction.py
+++ evs_xml_data_extraction.py
@@ -42,18 +42,130 @@ def clean_text(text):
 
 	return text
 
+def standartize_item_name(item_name):
+	item_name = re.sub("\.", "", item_name)
+	item_name = item_name.lower()
+	item_name = re.sub("q", "Q", item_name)
+
+	if '_'  in item_name:
+		item_name = item_name.split('_')
+		item_name = item_name[0]
+
+	return item_name
+
+def determine_survey_item_module(item_name, filename):
+	module = 'No module'
+	if 'Q' in item_name:
+		digits_in_item_name = re.sub("Q", "", item_name)
+		digits_in_item_name = re.sub("a", "", digits_in_item_name)
+		digits_in_item_name = re.sub("b", "", digits_in_item_name)
+		digits_in_item_name = re.sub("c", "", digits_in_item_name)
+		digits_in_item_name = re.sub("d", "", digits_in_item_name)
+		digits_in_item_name = re.sub("e", "", digits_in_item_name)
+		digits_in_item_name = re.sub("f", "", digits_in_item_name)
+
+		if '/' in digits_in_item_name:
+			digits_in_item_name = digits_in_item_name.split('/')
+			digits_in_item_name = digits_in_item_name[0]
+		# if '_'  in digits_in_item_name:
+		# 	digits_in_item_name = digits_in_item_name.split('_')
+		# 	digits_in_item_name = digits_in_item_name[0]
+
+
+		digits_in_item_name = int(digits_in_item_name)
+		
+
+
+		if '2008' in filename:
+			#QUESTIONS SUR LA VIE EN GÉNÉRAL, LES LOISIRS ET DE TRAVAIL
+			if digits_in_item_name < 22:
+				module = 'A'
+			#QUESTIONS SUR LE SENS DE LA VIE
+			if digits_in_item_name >= 22 and digits_in_item_name < 42:
+				module = 'B'
+			#LA FAMILLE ET DU MARIAGE
+			if digits_in_item_name >= 42 and digits_in_item_name < 54:
+				module = 'C'
+			#QUESTIONS SUR DES SUJETS DE SOCIETE
+			if digits_in_item_name >= 54 and digits_in_item_name < 86:
+				module = 'D'
+			#CARACTERISTIQUES DEMOGRAPHIQUES
+			if digits_in_item_name >= 86:
+				module = 'E'
+
+		if '1999' in filename:
+			#QUESTIONS SUR LA VIE EN GÉNÉRAL, LES LOISIRS
+			if digits_in_item_name < 13:
+				module = 'A'
+			#TRAVAIL
+			if digits_in_item_name >= 13 and digits_in_item_name < 21:
+				module = 'B'
+			#RELIGION
+			if digits_in_item_name >= 21 and  digits_in_item_name < 40:
+				module = 'C'
+			#LA FAMILLE ET DU MARIAGE
+			if digits_in_item_name >= 40 and digits_in_item_name < 51:
+				module = 'D'
+			#POLITIQUE
+			if digits_in_item_name >= 51 and digits_in_item_name < 64:
+				module = 'E'
+			#SOCIETE (not so sure about where it starts...)
+			if digits_in_item_name >= 64 and digits_in_item_name < 84:
+				module = 'F'
+			#CARACTERISTIQUES DEMOGRAPHIQUES
+			if digits_in_item_name >= 84:
+				module = 'G'
+
+	return module
+
 
 def main(filename):
 	sentence_splitter_prefix = 'tokenizers/punkt/'
 
-	if 'ENG' in filename:
+	if 'ENG_' in filename:
 		sentence_splitter_suffix = 'english.pickle'
-	if 'FRE' in filename:
+	if 'FRE_' in filename:
 		sentence_splitter_suffix = 'french.pickle'
 		country = 'France'
-	if 'GER' in filename:
+	if 'GER_' in filename:
 		sentence_splitter_suffix = 'german.pickle'
+	if 'CZE_' in filename:
+		sentence_splitter_suffix = 'czech.pickle'
+	if 'POR_' in filename:
+		sentence_splitter_suffix = 'portuguese.pickle'
+	if 'ITA_' in filename:
+		sentence_splitter_suffix = 'italian.pickle'
+	if 'RUS_' in filename:
+		sentence_splitter_suffix = 'russian.pickle'
+	if 'SPA_' in filename:
+		sentence_splitter_suffix = 'spanish.pickle'
+	if 'DAN_' in filename:
+		sentence_splitter_suffix = 'danish.pickle'
+	if 'DUT_' in filename:
+		sentence_splitter_suffix = 'dutch.pickle'
+	if 'SLO_' in filename:
+		sentence_splitter_suffix = 'slovene.pickle'
+	if 'EST_' in filename:
+		sentence_splitter_suffix = 'estonian.pickle'
+	if 'FIN_' in filename:
+		sentence_splitter_suffix = 'finnish.pickle'
+	if 'GRE_' in filename:
+		sentence_splitter_suffix = 'greek.pickle'
+	if 'POL_' in filename:
+		sentence_splitter_suffix = 'polish.pickle'
+	if 'NOR_' in filename:
+		sentence_splitter_suffix = 'norwegian.pickle'
+	if 'SWE_' in filename:
+		sentence_splitter_suffix = 'swedish.pickle'
+	if 'TUR_' in filename:
+		sentence_splitter_suffix = 'turkish.pickle'
+	if 'HUN_' in filename:
+		sentence_splitter_suffix = 'hungarian.pickle'
 
+
+		
+ 	
+	#Punkt Sentence Tokenizer from NLTK	
 	sentence_splitter = sentence_splitter_prefix+sentence_splitter_suffix
 	tokenizer = nltk.data.load(sentence_splitter)
 
@@ -67,7 +179,7 @@ def main(filename):
 	evs_vars = root.findall('.//dataDscr/var')
 
 	survey_id = filename.replace('.xml', '')
-	df_survey_item = pd.DataFrame(columns=['survey_itemid', 'item_type', 'item_name', 'item_value', 'text',  'item_is_source'])
+	df_survey_item = pd.DataFrame(columns=['survey_itemid', 'module','item_type', 'item_name', 'item_value', 'text',  'item_is_source'])
 
 	
 
@@ -83,13 +195,18 @@ def main(filename):
 			else:
 				elem_item_name = var.find('labl').text
 				item_name = elem_item_name[elem_item_name.find("(")+1:elem_item_name.find(")")]
+
+			item_name = standartize_item_name(item_name)
+			# module = 'No module'
 			
 			if node.tag=='preQTxt':
 				text = clean_text(node.text)
 				item_type = 'INTRO'
+				module = determine_survey_item_module(item_name, filename)
+
 				split_into_sentences = tokenizer.tokenize(text)
 				for item in split_into_sentences:
-					data = {"survey_itemid": update_item_id(survey_id), 'item_type': item_type, 
+					data = {"survey_itemid": update_item_id(survey_id), 'module': module,'item_type': item_type, 
 					'item_name': item_name, 'item_value': item_value,  'text': item, 'item_is_source': False}
 					df_survey_item = df_survey_item.append(data, ignore_index = True)
 					last_tag = node.tag
@@ -97,17 +214,20 @@ def main(filename):
 			if node.tag=='ivuInstr':
 				text = clean_text(node.text)
 				item_type = 'INSTRUCTION'
+				module = determine_survey_item_module(item_name, filename)
+
 				split_into_sentences = tokenizer.tokenize(text)
 				for item in split_into_sentences:
-					data = {"survey_itemid": update_item_id(survey_id), 'item_type': item_type, 
+					data = {"survey_itemid": update_item_id(survey_id), 'module': module,'item_type': item_type, 
 					'item_name': item_name, 'item_value': item_value,  'text': item, 'item_is_source': False}
 					df_survey_item = df_survey_item.append(data, ignore_index = True)
 					last_tag = node.tag
 
 			if node.tag=='qstnLit':
 				text = clean_text(node.text)
+				module = determine_survey_item_module(item_name, filename)
 				if text in initial_request:
-					data = {"survey_itemid": update_item_id(survey_id), 'item_type': 'REQUEST', 
+					data = {"survey_itemid": update_item_id(survey_id), 'module': module,'item_type': 'REQUEST', 
 						'item_name': 'INTRO', 'item_value': item_value,  'text': text, 'item_is_source': False}
 					df_survey_item = df_survey_item.append(data, ignore_index = True)
 					last_tag = node.tag
@@ -115,7 +235,7 @@ def main(filename):
 					item_type = 'REQUEST'
 					split_into_sentences = tokenizer.tokenize(text)
 					for item in split_into_sentences:
-						data = {"survey_itemid": update_item_id(survey_id), 'item_type': item_type, 
+						data = {"survey_itemid": update_item_id(survey_id), 'module': module,'item_type': item_type, 
 						'item_name': item_name, 'item_value': item_value,  'text': item, 'item_is_source': False}
 						df_survey_item = df_survey_item.append(data, ignore_index = True)
 						last_tag = node.tag
@@ -124,12 +244,14 @@ def main(filename):
 				text = clean_text(node.text)
 				print(text, last_tag)
 				item_type = 'REQUEST'
-				if 'level' in node.attrib:
-					item_name = node.attrib['level']
+				module = determine_survey_item_module(item_name, filename)
+				# if 'level' in node.attrib:
+				# 	item_name = node.attrib['level']
+				# 	item_name = standartize_item_name(item_name)
 				if 'sample' not in text and text != country: 
 					split_into_sentences = tokenizer.tokenize(text)
 					for item in split_into_sentences:
-						data = {"survey_itemid": update_item_id(survey_id), 'item_type': item_type, 
+						data = {"survey_itemid": update_item_id(survey_id), 'module': module, 'item_type': item_type, 
 						'item_name': item_name, 'item_value': item_value,  'text': item, 'item_is_source': False}
 						df_survey_item = df_survey_item.append(data, ignore_index = True)
 						last_tag = node.tag
@@ -140,11 +262,12 @@ def main(filename):
 					text = clean_text(txt.text)
 					if 'sample' not in text and text != country: 
 						item_type = 'ANSWER'
+						module = determine_survey_item_module(item_name, filename)
 						catValu = node.find('catValu')
 						item_value = catValu.text
 						split_into_sentences = tokenizer.tokenize(text)
 						for item in split_into_sentences:
-							data = {"survey_itemid": update_item_id(survey_id), 'item_type': item_type, 
+							data = {"survey_itemid": update_item_id(survey_id), 'module': module, 'item_type': item_type, 
 							'item_name': item_name, 'item_value': item_value,  'text': item, 'item_is_source': False}
 							df_survey_item = df_survey_item.append(data, ignore_index = True)
 							last_tag = node.tag
@@ -153,7 +276,7 @@ def main(filename):
 	
 
 
-	df_survey_item.to_csv('test.csv')
+	df_survey_item.to_csv(str(filename)+'.csv')
 
 		
 
@@ -162,7 +285,7 @@ def main(filename):
 
 if __name__ == "__main__":
 	#Call script using filename. 
-	#For instance: reset && python3 evs_xml_data_extraction.py EVS_FRE_FR_R04_2008.xml
+	#For instance: reset && python3 evs_xml_data_extraction.py EVS_R03_1999_FRE_FR.xml
 	filename = str(sys.argv[1])
-	print("Executing data extraction script for EVS 2008 (xml files)")
+	print("Executing data extraction script for EVS 1999/2008 (xml files)")
 	main(filename)
