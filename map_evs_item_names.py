3ad9b86b9aa45142cc26274d58e2c80c2dbc8994
dsorato <danielly.sorato@gmail.com>
2020-04-20 16:42:14 +0200

    waiting confirmation if thats what is needed

--- map_evs_item_names.py
+++ map_evs_item_names.py
@@ -2,6 +2,7 @@ import xml.etree.ElementTree as ET
 import sys
 import re
 import string
+import pandas as pd
 
 
 def main(filename_xlsx,filename_xml):
@@ -10,8 +11,15 @@ def main(filename_xlsx,filename_xml):
 	tree = ET.parse(file)
 	root = tree.getroot()
 
+	xlsx_file = pd.read_excel(filename_xlsx)
+	xlsx_file['v_item'] = xlsx_file['v_item'].str.lower()
+	print(xlsx_file)
+	items_xlsx = xlsx_file['v_item']
+
 	parent_map = dict((c, p) for p in tree.getiterator() for c in p)
 	evs_vars = root.findall('.//dataDscr/var')
+
+	item_list = []
 
 	for var in evs_vars:
 		for node in var.getiterator():
@@ -24,7 +32,24 @@ def main(filename_xlsx,filename_xml):
 				item_name = qstn.attrib['seqNo'] 
 
 			if item_name != '':
-				print(var.attrib['name'], item_name)
+				item = [var.attrib['name'].lower(), item_name]
+				if item not in item_list:
+					item_list.append(item)
+
+	print(item_list)
+
+	df_evs_sample = pd.DataFrame(columns=['v_item', 'q_item'])
+	for item_x in items_xlsx:
+		for item_y in item_list:
+			if item_x == item_y[0]:
+				print(item_x, item_y[0], item_y[1].replace('.', ''))
+				data = {'v_item':item_y[0], 'q_item': item_y[1].replace('.', '')}
+				df_evs_sample = df_evs_sample.append(data, ignore_index=True)
+
+	df_evs_sample= df_evs_sample.drop_duplicates(subset='v_item', keep="last")
+	result = pd.merge(df_evs_sample, xlsx_file, on='v_item')
+	result.to_csv('evs_data/df_evs_sample.csv', encoding='utf-8', index=False)
+
 
 
 
