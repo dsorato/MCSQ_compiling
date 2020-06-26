5d91826d09916a2ed2de295d4e71be2b3fea2ef3
dsorato <danielly.sorato@gmail.com>
2020-06-26 17:00:36 +0200

    wip

--- include_alignments_in_database.py
+++ include_alignments_in_database.py
@@ -27,7 +27,7 @@ def retrieve_based_on_item_type(text, item_type, survey_itemid):
 		if item_id is not None:
 			items_to_include = get_survey_item_info_from_id(item_id, 'introductionid', survey_itemid)
 	elif item_type == 'RESPONSE':
-		item_id =  get_response_id(text)
+		item_id =  get_response_id(text, survey_itemid)
 		if item_id is not None:
 			items_to_include = get_survey_item_info_from_id(item_id, 'responseid', survey_itemid)
 
