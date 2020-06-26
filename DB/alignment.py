89ce7e124bb4d34d38eff40ec8096a2881ef5150
dsorato <danielly.sorato@gmail.com>
2020-06-26 14:58:32 +0200

    wip

--- DB/alignment.py
+++ DB/alignment.py
@@ -21,11 +21,10 @@ class Alignment(Base):
     source_survey_item_elementid = Column(Integer, ForeignKey('survey_item.survey_item_elementid'))
     target_survey_item_elementid = Column(Integer, ForeignKey('survey_item.survey_item_elementid'))
 
-   	survey_item = relationship("Survey_item", backref="survey_item")
+    survey_item = relationship("Survey_item", backref="survey_item")
    
 
-    def __init__(self,source_text, target_text, source_survey_itemid, target_survey_itemid, 
-    	source_survey_item_elementid,target_survey_item_elementid):
+    def __init__(self,source_text, target_text, source_survey_itemid, target_survey_itemid, source_survey_item_elementid,target_survey_item_elementid):
         self.source_text = source_text
         self.target_text = target_text
         self.source_survey_itemid = source_survey_itemid
