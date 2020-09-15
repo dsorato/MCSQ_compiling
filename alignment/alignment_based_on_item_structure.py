b3a749930819c57b2676f6863dc4206c30bced2c
danielly <danielly@localhost.localdomain>
2020-09-15 18:36:11 +0200

    Response alignment and other item types with same number of segments is woeking well. Now it is necessary to refactor the difficult cases (different number of segments)

--- alignment/alignment_based_on_item_structure.py
+++ alignment/alignment_based_on_item_structure.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import sys
 import os
+from countryspecificrequest import *
 import math
 from difflib import SequenceMatcher
 
@@ -9,31 +10,6 @@ from difflib import SequenceMatcher
 
 # def similar(a, b):
 #     return SequenceMatcher(None, a, b).ratio()
-
-# def get_country_and_language(df, language):
-# 	unique_survey_item_ids = df.survey_item_ID.unique()
-# 	unique_country_language = []
-# 	for u in unique_survey_item_ids:
-# 		get_country = u.split(language+'_')[1]
-# 		country = get_country.split('_')[0]
-# 		country_language = language+'_'+country
-# 		if country_language not in unique_country_language:
-# 			unique_country_language.append(country_language)
-
-# 	return unique_country_language
-
-# def filter_dataframe_by_round(df, study):
-# 	return df[df['Study'].str.contains(study)]
-
-# def filter_eng_version(df, eng):
-# 	return df[df['survey_item_ID'].str.contains(eng)]
-
-
-
-# def filter_by_item_name(df_source, df_target):
-# 	return pd.concat([df_source, df_target], sort=False)
-# 	# return pd.merge(df_source, df_target, on = 'item_name')
-
 
 
 # def align_more_segments_in_source(df, df_source, df_target, target_language):
@@ -89,93 +65,82 @@ from difflib import SequenceMatcher
 # 			'source':None, 'target':irow[target_language], 'item_value': None,
 # 			'source_survey_itemID': None, 'target_survey_itemID': irow['survey_item_ID']}
 # 			df = df.append(data, ignore_index=True)
-
-
-# 	return df
-# def align_responses(df_source, df_target, target_language, item_type):
-# 	df = pd.DataFrame(columns=['item_name', 'item_type', 'source', 'target', 'item_value', 
-# 		'source_survey_itemID', 'target_survey_itemID'])
-# 	df_source = df_source[df_source['item_type']==item_type]
-# 	df_target = df_target[df_target['item_type']==item_type]
-
-# 	for i, irow in df_source.iterrows():
-# 		for j, jrow in df_target.iterrows():
-# 			if jrow['item_type'] == irow['item_type'] and jrow['item_name'] == irow['item_name']:
-# 				if jrow['item_type'] == 'RESPONSE':
-# 					value_i, value_j = cast_item_value(irow['item_value'], jrow['item_value'])
-# 					if value_j == value_i and jrow[target_language] not in df['target'].unique() and irow['ENG_SOURCE'] not in df['source'].unique():
-# 						data = {'item_name':jrow['item_name'], 'item_type':jrow['item_type'], 
-# 						'source':irow['ENG_SOURCE'], 'target':jrow[target_language], 'item_value': jrow['item_value'], 
-# 						'source_survey_itemID': irow['survey_item_ID'], 'target_survey_itemID': jrow['survey_item_ID']}
-# 						df = df.append(data, ignore_index=True)
-
-# 	return df
-
-# def align_remaining(df_source, df_target, target_language, item_type):
-# 	df = pd.DataFrame(columns=['item_name', 'item_type', 'source', 'target', 'item_value', 
-# 		'source_survey_itemID', 'target_survey_itemID'])
-# 	df_source = df_source[df_source['item_type']==item_type]
-# 	df_target = df_target[df_target['item_type']==item_type]
-
-# 	if df_source.empty:
-# 		for i,row in df_target.iterrows():
-# 			data = {'item_name': row['item_name'], 'item_type':row['item_type'], 
-# 					'source':None, 'target':row[target_language], 'item_value': None,
-# 					'source_survey_itemID': None, 'target_survey_itemID': row['survey_item_ID']}
-# 			df = df.append(data, ignore_index=True)
-# 		return df
-# 	if df_target.empty:
-# 		for i,row in df_source.iterrows():
-# 			data = {'item_name': row['item_name'], 'item_type':row['item_type'], 
-# 					'source':row['ENG_SOURCE'], 'target':None, 'item_value': None,
-# 					'source_survey_itemID': row['survey_item_ID'], 'target_survey_itemID': None}
-# 			df = df.append(data, ignore_index=True)
-# 		return df
-# 	else:
-# 		if len(df_source)> len(df_target):
-# 			df = align_more_segments_in_source(df, df_source, df_target, target_language)
-
-# 		elif len(df_target)> len(df_source):
-# 			df = align_more_segments_in_target(df, df_source, df_target, target_language)
-# 		else:
-# 			for i, irow in df_source.iterrows():
-# 				for j, jrow in df_target.iterrows():
-# 					if  jrow['item_name'] == irow['item_name']:
-# 						if jrow[target_language] not in df['target'].unique() and irow['ENG_SOURCE'] not in df['source'].unique():
-# 								data = {'item_name':jrow['item_name'], 'item_type':jrow['item_type'], 
-# 								'source':irow['ENG_SOURCE'], 'target':jrow[target_language], 'item_value': None,
-# 								'source_survey_itemID': irow['survey_item_ID'], 'target_survey_itemID': jrow['survey_item_ID']}
-# 								df = df.append(data, ignore_index=True)
-
-
-
-# 	return df
-
-
-# def cast_item_value(value_i, value_j):
-# 	if isinstance(value_i, float) and math.isnan(value_i) == False:
-# 		value_i = int(value_i)
-# 	if isinstance(value_j, float) and math.isnan(value_j) == False:
-# 		value_j = int(value_j)
-
-# 	value_i = str(value_i)
-# 	value_j = str(value_j)
-
-# 	return value_i, value_j
+#  	return df
 
 
 
 
-def align_on_metadata(df, df_source, df_target):
+def align_introduction_instruction_request(df, df_source, df_target, item_type):
+	df_source = df_source[df_source['item_type']==item_type]
+	df_target = df_target[df_target['item_type']==item_type]
 
-	df_introduction = align_remaining(df1, df2, target_language, 'INTRODUCTION')
-	df = df.append(df_introduction, ignore_index=True)
-	df_instruction = align_remaining(df1, df2, target_language, 'INSTRUCTION')
-	df = df.append(df_instruction, ignore_index=True)
-	df_request = align_remaining(df1, df2, target_language, 'REQUEST')
-	df = df.append(df_request, ignore_index=True)
-	df_response = align_responses(df1, df2, target_language, 'RESPONSE')
-	df = df.append(df_response, ignore_index=True)
+	if df_source.empty:
+		for i,row in df_target.iterrows():
+			data = {'source_survey_itemID': None, 'target_survey_itemID': row['survey_item_ID'], 'Study': row['Study'], 
+			'module': row['module'], 'item_type': item_type, 'item_name':row['item_name'], 'item_value': None, 
+			'source_text': None, 'target_text': row['text']}
+			df = df.append(data, ignore_index=True)
+		return df
+
+	if df_target.empty:
+		for i,row in df_source.iterrows():
+			data = {'source_survey_itemID': row['survey_item_ID'], 'target_survey_itemID': None , 'Study': row['Study'], 
+			'module': row['module'], 'item_type': item_type, 'item_name':row['item_name'], 'item_value': None, 
+			'source_text': row['text'], 'target_text': None}
+			df = df.append(data, ignore_index=True)
+		return df
+	else:
+		list_target = df_target.values.tolist()
+		list_source = df_source.values.tolist()
+
+		if len(list_source) > len(df_target):
+			pass
+			# df = align_more_segments_in_source(df, df_source, df_target, target_language)
+
+		elif len(list_target) > len(list_source):
+			pass
+			# df = align_more_segments_in_target(df, df_source, df_target, target_language)
+		elif len(list_target) == len(list_source):
+			
+			for i, item in enumerate(list_source):
+				print(item, item[0],  list_target[i][0])
+				data = {'source_survey_itemID': item[0], 'target_survey_itemID': list_target[i][0] , 'Study': item[1], 
+				'module': item[2], 'item_type': item_type, 'item_name':item[4], 'item_value': None, 
+				'source_text': item[6], 'target_text':  list_target[i][6]}
+				df = df.append(data, ignore_index=True)
+
+						
+
+
+	return df
+
+
+def align_responses(df, df_source, df_target):
+	df_source = df_source[df_source['item_type']=='RESPONSE']
+	df_target = df_target[df_target['item_type']=='RESPONSE']
+
+	df_merge = pd.merge(df_source, df_target, on='item_value')
+
+	for i, row in df_merge.iterrows():
+		data = {'source_survey_itemID': row['survey_item_ID_x'], 'target_survey_itemID':  row['survey_item_ID_y'], 'Study': row['Study_x'], 
+		'module': row['module_x'], 'item_type': 'RESPONSE', 'item_name':row['item_name_x'], 'item_value': row['item_value'], 
+		'source_text': row['text_x'], 'target_text': row['text_y']}
+		df = df.append(data, ignore_index=True)
+
+	return df
+
+"""
+Calls the appropriate method for alignment based on metadata.
+Responses are aligned separately of other item types because answers are merged using the item value.
+"""
+def align_on_metadata(df, df_source, df_target, process_responses):
+
+	df = align_introduction_instruction_request(df, df_source, df_target, 'INTRODUCTION')
+	df = align_introduction_instruction_request(df, df_source, df_target, 'INSTRUCTION')
+	df = align_introduction_instruction_request(df, df_source, df_target, 'REQUEST')
+	if process_responses:
+		df = align_responses(df, df_source, df_target)
+	
 
 	return df
 
@@ -226,9 +191,27 @@ Returns:
 def get_target_language_country_metadata(filename):
 	filename_without_extension = filename.replace('.csv', '')
 	filename_without_extension = filename_without_extension.split('_')
-	target_language_country = filename[3]+'_'+filename[4]
+	target_language_country = filename_without_extension[3]+'_'+filename_without_extension[4]
 	
 	return target_language_country
+
+"""
+Instantiates the appropriate set of country-specific requests according to the study.
+Country-specific requests are deleted from alignment by design because the answer categories
+frequently change from country to country.
+
+Args:
+	param1 study (string): study metadata, embedded in filenames.
+
+Returns:
+	country_specific_requests (Python object). Instance of python object that encapsulates the item names of 
+	teh country specific questions.
+"""
+def instantiate_country_specific_request_object(study):
+	if 'ESS_R01' in study:
+		country_specific_requests = ESSCountrySpecificR01()
+
+	return country_specific_requests
 
 def main(folder_path, filename_source, filename_target):
 	path = os.chdir(folder_path)
@@ -246,6 +229,9 @@ def main(folder_path, filename_source, filename_target):
 	else:
 		source_language_country = 'ENG_SOURCE'
 
+
+	country_specific_requests = instantiate_country_specific_request_object(study)
+
 	"""
 	Computes the intersection between the modules of source and target questionnaires.
 	We are only interested in aligning modules that are present in both files.
@@ -256,17 +242,20 @@ def main(folder_path, filename_source, filename_target):
 		
 		unique_item_names_source = df_source_filtered.item_name.unique()
 		unique_item_names_target = df_target_filtered.item_name.unique()
-		for unique in unique_item_name_source:
-			print(unique)
+		
+		for unique_item_name in unique_item_names_source:
+			process_responses = True
+			if unique_item_name.lower() in country_specific_requests.item_names:
+				process_responses = False
 			"""
 			Computes the intersection between the item names of source and target questionnaires.
 			We are only interested in aligning questions that are present in both files.
 			"""
-			df_source_by_item_name = df_source_filtered[df_source_filtered['item_name'].lower()==unique.lower()]
-			df_target_by_item_name = df_target_filtered[df_target_filtered['item_name'].lower()==unique.lower()]
+			df_source_by_item_name = df_source_filtered[df_source_filtered['item_name'].str.lower()==unique_item_name.lower()]
+			df_target_by_item_name = df_target_filtered[df_target_filtered['item_name'].str.lower()==unique_item_name.lower()]
 			
 			if df_target_by_item_name.empty == False and df_source_by_item_name.empty == False:
-				df = align_on_metadata(df, df_source_by_item_name, df_target_by_item_name)
+				df = align_on_metadata(df, df_source_by_item_name, df_target_by_item_name, process_responses)
 				
 	
 	df.to_csv(source_language_country+'_'+target_language_country+'_'+study+'.csv', encoding='utf-8', index=False)
@@ -276,8 +265,7 @@ def main(folder_path, filename_source, filename_target):
 
 
 if __name__ == "__main__":
-	#Call script using the filenames of two files that should be aligned 
-	#python3 alignment_based_on_item_structure.py ./test_data/ESS_R01_2002_ENG_SOURCE.csv ./test_data/ESS_R01_2002_CAT_ES.csv 
-	filename_source= str(sys.argv[1])
-	filename_target = str(sys.argv[2])
-	main(filename_source,filename_target)
+	folder_path = str(sys.argv[1])
+	filename_source = str(sys.argv[2])
+	filename_target = str(sys.argv[3])
+	main(folder_path, filename_source,filename_target)
---
+++ alignment/countryspecificrequest.py
@@ -0,0 +1,3 @@
+class ESSCountrySpecificR01():
+	item_names = ['B14', 'B25b', 'B27', 'C10', 'C12', 'F6']
+	item_names = [x.lower() for x in item_names]
