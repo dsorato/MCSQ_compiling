17881b25c684986107a8b556b548198628fc4e4d
dsorato <danielly.sorato@gmail.com>
2020-10-09 11:49:31 +0200

    Modifications in Survey_item table: dropped survey_element_id, modified response FK, added text column for redudancy

--- DB/survey_item.py
+++ DB/survey_item.py
@@ -12,38 +12,35 @@ from sqlalchemy.orm import relationship, backref
 class Survey_item(Base):
     __tablename__ = 'survey_item'
 
-    # emits CREATE SEQUENCE + INTEGER
-    survey_item_elementid = Column(Integer, Sequence('survey_item_element_id_seq'), primary_key=True)
     survey_itemid = Column(String, primary_key=True)
     surveyid = Column(String, ForeignKey('survey.surveyid'), nullable=False)
+    text = Column(String)
     moduleid = Column(Integer, ForeignKey('module.moduleid'))
     requestid = Column(Integer, ForeignKey('request.requestid'))
-    responseid = Column(Integer)
-    response_item_id = Column(Integer)
+    responseid = Column(Integer, ForeignKey('response.responseid'))
     instructionid = Column(Integer, ForeignKey('instruction.instructionid'))
     introductionid = Column(Integer, ForeignKey('introduction.introductionid'))
     country_language = Column(String, nullable=False)
     item_is_source = Column(Boolean)
     item_name = Column(String)
     item_type = Column(String)
-    ForeignKeyConstraint(['responseid', 'response_item_id'], ['response.responseid','response.response_item_id'])
-
 
     survey = relationship("Survey", backref=backref("survey_item", uselist=False))
     module = relationship("Module", backref="module")
     request = relationship("Request", backref="request")
+    response = relationship("Response", backref="response")
     instruction = relationship("Instruction", backref="instruction")
     introduction = relationship("Introduction", backref="introduction")
 
 
 
-    def __init__(self, survey_itemid, surveyid, moduleid, requestid, responseid, response_item_id, instructionid,introductionid, country_language, item_is_source, item_name, item_type):
-        self.survey_itemid = survey_itemid
+    def __init__(self, surveyid, text, moduleid, requestid, responseid, instructionid, introductionid, 
+        country_language, item_is_source, item_name, item_type):
+        
         self.surveyid = surveyid
         self.moduleid = moduleid
         self.requestid = requestid
         self.responseid = responseid
-        self.response_item_id = response_item_id
         self.instructionid = instructionid
         self.introductionid = introductionid
         self.country_language = country_language
