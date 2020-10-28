dff69b6ae8bf9ad97f06c1e40f69cc95285d9043
dsorato <danielly.sorato@gmail.com>
2020-10-28 15:49:55 +0100

    Extracting answers

--- preprocessing/evs_data_extraction.py
+++ preprocessing/evs_data_extraction.py
@@ -64,192 +64,6 @@ def remove_trailing(clean):
 	return without_trailing
 
 
-
-def extract_response_types(response_types, a_response_type):
-	ret = ''
-	filtered_response_types_df = response_types[response_types['Code'] == a_response_type]
-
-	if a_response_type in response_types_dict:
-		ret = response_types_dict[a_response_type]
-
-	else:
-		if filtered_response_types_df.empty:
-			ret = ''
-		else:
-			translated_cells = iter(filtered_response_types_df.Translated)
-			translated_cells = list(translated_cells)
-			
-			if pd.notna(translated_cells).all() and 'Translation' not in translated_cells:
-				ret = translated_cells
-			# else:
-			# 	ret = iter(filtered_response_types_df.TranslatableElement)
-			# 	ret = list(ret)
-
-				clean_text_ret = []
-				for item in ret:
-					item = clean_text(item)
-					clean_text_ret.append(item)
-
-			
-				response_types_dict[a_response_type] = clean_text_ret
-
-	return ret
-
-def extract_constant(constants, a_constant):
-	ret = ''
-	filtered_constants_df = constants[constants['Code'] == a_constant]
-
-	if a_constant in constants_dict:
-		ret = constants_dict[a_constant]
-
-	else:
-		if filtered_constants_df.empty:
-			ret = ''
-		else:
-			translated_cell = iter(filtered_constants_df.Translation)
-			translated_cell = list(translated_cell)[0]
-			
-			if pd.notna(translated_cell) and translated_cell != 'Translation':
-				ret = translated_cell
-			# else:
-			# 	ret = iter(filtered_constants_df.TranslatableElement)
-			# 	ret = list(ret)[0]
-
-				ret = clean_text(ret)
-				constants_dict[a_constant] = ret
-
-	return ret
-
-def check_question_name(question_name):
-	ret = ''
-	if isinstance(question_name, str):
-		if 'INTRODUCTION' in question_name:
-			ret =  'INTRODUCTION'
-		else:
-			ret =  'REQUEST'
-
-	return ret
-
-def check_item_name(row):
-	if isinstance(row['VariableName'], str):
-		ret = row['VariableName']
-	else:
-		ret =  row['QuestionName'] 
-
-	return ret
-
-def decide_item_type_other(row):
-	if 'CodInstruction' in str(row['QuestionElement']):
-		item_type = 'INSTRUCTION'
-	elif ('INTRO' or 'SECTION') in str(row['QuestionName']):
-		item_type = 'INTRODUCTION'
-	else:
-		item_type = 'REQUEST'
-
-	return item_type
-
-def decide_item_type_constant(constant, row):
-	if constant in instruction_constants:
-		item_type = 'INSTRUCTION'
-	else:
-		item_type = check_question_name(row['QuestionName'])
-
-	return item_type
-					
-
-def replace_intro_in_item_name(column_item_name, item_name):
-	item_name_index = column_item_name.index(item_name)
-	if 'INTRO' in item_name:
-		for i, value in enumerate(column_item_name[item_name_index:]):
-			if 'INTRO' not in value and 'SECTION' not in value:
-				item_name = value
-				break
-
-	return item_name
-
-def process_questionnaire(questionnaire, constants, response_types, df_questionnaire, survey_item_prefix, study, splitter):
-	for index, row in questionnaire.iterrows(): 
-		"""
-		case 1: Translated row is not null = We have the tranlated text
-		"""
-		if pd.isna(row['Translated']) == False and pd.isna(row['QuestionName']) == False:
-			if row['QuestionName'] != 'IWER_INTRO' and row['QuestionName'] != 'INTRO0' and 'SECTION' not in row['QuestionName']:
-				"""
-				item is a constant. A constant can be type: INSTRUCTION, INTRODUCTION or REQUEST
-				"""
-				if pd.notna(row['Translated']) and row['QuestionElement'] == 'Constant' and row['Translated'] not in response_constants:
-					constant = row['Translated']
-					text = extract_constant(constants, constant)
-					column_item_name = list(questionnaire['QuestionName'])
-					item_name = replace_intro_in_item_name(column_item_name, row['QuestionName']) 
-					
-					if text != '':
-						if df_questionnaire.empty:
-							survey_item_id = ut.get_survey_item_id(survey_item_prefix)
-						else:
-							survey_item_id = ut.update_survey_item_id(survey_item_prefix)
-
-						data = {'survey_item_ID':survey_item_id, 'Study':study, 'module': row['Module'], 
-						'item_type':decide_item_type_constant(constant, row), 'item_name': item_name, 
-						'item_value':None, 'text':clean_text(text)}
-						df_questionnaire = df_questionnaire.append(data, ignore_index = True)		
-				elif pd.notna(row['Translated']) and (row['QuestionElement'] == 'AnswerType' or row['QuestionElement'] == 'Answer' or row['Translated'] in response_constants):
-					"""
-					item is a response. A response can be only type RESPONSE
-					"""		
-					response = row['Translated']
-					if response in response_constants:
-						text = extract_constant(constants, response)
-						if text != '':
-							survey_item_id = ut.update_survey_item_id(survey_item_prefix)
-							data = {'survey_item_ID':survey_item_id, 'Study':study, 'module': row['Module'], 
-							'item_type':decide_item_type_constant(constant, row), 'item_name': row['QuestionName'], 
-							'item_value':None, 'text':clean_text(text)}
-							df_questionnaire = df_questionnaire.append(data, ignore_index = True)
-					#response != 'Translation' is a workaround to deal with incorrecly translated segments
-					elif row['QuestionElement'] == 'Answer' and response != 'Translation':
-						if text != '':
-							if df_questionnaire.empty:
-								survey_item_id = ut.get_survey_item_id(survey_item_prefix)
-							else:
-								survey_item_id = ut.update_survey_item_id(survey_item_prefix)
-
-							data = {'survey_item_ID':survey_item_id, 'Study':study, 'module': row['Module'], 
-							'item_type':decide_item_type_constant(constant, row), 'item_name': item_name, 
-							'item_value':None, 'text':clean_text(text)}
-							df_questionnaire = df_questionnaire.append(data, ignore_index = True)
-					else:
-						text = extract_response_types(response_types, text)
-						if text != '':
-							for item in text:
-								if item != 'Translation':
-
-									survey_item_id = ut.update_survey_item_id(survey_item_prefix)
-
-									data = {'survey_item_ID':survey_item_id, 'Study':study, 'module': row['Module'], 
-									'item_type': 'RESPONSE', 'item_name': row['QuestionName'], 
-									'item_value':dk_nr_standard(row['QuestionElementNr']), 'text':clean_text(item)}
-									df_questionnaire = df_questionnaire.append(data, ignore_index = True)			
-				# item type can be INTRODUCTION, INSTRUCTION or REQUEST
-				else:
-					if pd.notna(row['Translated']):
-						if df_questionnaire.empty:
-							survey_item_id = ut.get_survey_item_id(survey_item_prefix)
-						else:
-							survey_item_id = ut.update_survey_item_id(survey_item_prefix)
-
-						text = clean_text(row['Translated'])
-						column_item_name = list(questionnaire['QuestionName'])
-						item_name = replace_intro_in_item_name(column_item_name, row['QuestionName']) 
-						sentences = splitter.tokenize(text)
-
-						for sentence in sentences:
-							data = {'survey_item_ID':survey_item_id, 'Study':study, 'module': row['Module'], 
-							'item_type':decide_item_type_other(row), 'item_name': item_name, 
-							'item_value':None, 'text':clean_text(sentence)}
-							df_questionnaire = df_questionnaire.append(data, ignore_index = True)		
-	return df_questionnaire
-
 def set_initial_structures(filename):
 	"""
 	Set initial structures that are necessary for the extraction of each questionnaire.
@@ -374,17 +188,63 @@ def process_request_segment(row, study, survey_item_prefix, splitter, df_questio
 	else:
 		text = row['Translated'] 
 
-		if df_questionnaire.empty:
-			survey_item_id = ut.get_survey_item_id(survey_item_prefix)
-		else:
-			survey_item_id = ut.update_survey_item_id(survey_item_prefix)
+	if df_questionnaire.empty:
+		survey_item_id = ut.get_survey_item_id(survey_item_prefix)
+	else:
+		survey_item_id = ut.update_survey_item_id(survey_item_prefix)
 
-		sentences = splitter.tokenize(text)
-		for sentence in sentences:
-			data = {'survey_item_ID':survey_item_id, 'Study':study, 'module': row['Module'], 
-			'item_type':'REQUEST', 'item_name': row['QuestionName'], 
-			'item_value':None, 'text':clean_text(sentence)}
-			df_questionnaire = df_questionnaire.append(data, ignore_index = True)	
+	sentences = splitter.tokenize(text)
+	for sentence in sentences:
+		data = {'survey_item_ID':survey_item_id, 'Study':study, 'module': row['Module'], 
+		'item_type':'REQUEST', 'item_name': row['QuestionName'], 
+		'item_value':None, 'text':clean_text(sentence)}
+		df_questionnaire = df_questionnaire.append(data, ignore_index = True)	
+
+	return df_questionnaire
+
+def process_instruction_segment(row, study, survey_item_prefix, splitter, df_questionnaire):
+	if isinstance(row['Translated'], float) or row['Translated'] == 'Translation':
+		if isinstance(row['TranslatableElement'], float):
+			return df_questionnaire
+		else:
+			text = row['TranslatableElement'] 
+	else:
+		text = row['Translated'] 
+
+	if df_questionnaire.empty:
+		survey_item_id = ut.get_survey_item_id(survey_item_prefix)
+	else:
+		survey_item_id = ut.update_survey_item_id(survey_item_prefix)
+
+	sentences = splitter.tokenize(text)
+	for sentence in sentences:
+		data = {'survey_item_ID':survey_item_id, 'Study':study, 'module': row['Module'], 
+		'item_type':'INSTRUCTION', 'item_name': row['QuestionName'], 
+		'item_value':None, 'text':clean_text(sentence)}
+		df_questionnaire = df_questionnaire.append(data, ignore_index = True)	
+
+	return df_questionnaire
+
+
+def process_response_segment(row, study, survey_item_prefix, df_questionnaire):
+	if isinstance(row['Translated'], float) or row['Translated'] == 'Translation':
+		if isinstance(row['TranslatableElement'], float):
+			return df_questionnaire
+		else:
+			text = row['TranslatableElement'] 
+	else:
+		text = row['Translated'] 
+
+	if df_questionnaire.empty:
+		survey_item_id = ut.get_survey_item_id(survey_item_prefix)
+	else:
+		survey_item_id = ut.update_survey_item_id(survey_item_prefix)
+
+
+	data = {'survey_item_ID':survey_item_id, 'Study':study, 'module': row['Module'], 
+	'item_type':'RESPONSE', 'item_name': row['QuestionName'], 
+	'item_value':row['QuestionElementNr'], 'text':clean_text(text)}
+	df_questionnaire = df_questionnaire.append(data, ignore_index = True)	
 
 	return df_questionnaire
 
@@ -407,12 +267,16 @@ def main(folder_path):
 			questionnaire['Translated'] = questionnaire['Translated'].replace(['DK'],'DKext')
 			# questionnaire = questionnaire[questionnaire['Translated'].notna()]
 			
-
 			for i, row in questionnaire.iterrows():
 				if row['QuestionElement'] == 'Constant':
 					df_questionnaire = process_constant_segment(constants, row, study, survey_item_prefix, splitter, df_questionnaire)
-				elif row['QuestionElement'] in ['QInstruction', 'QItemInstruction', 'QItem']:
+				elif row['QuestionElement'] in ['QInstruction', 'QItemInstruction', 'QItem'] and row['QuestionName'] not in ['INTRO0', 'INTRO1']:
 					df_questionnaire = process_request_segment(row, study, survey_item_prefix, splitter, df_questionnaire)
+				elif row['QuestionElement'] == 'IWER':
+					if row['QuestionName'] != 'IWER_INTRO' and isinstance(row['QuestionName'], str):
+						df_questionnaire = process_instruction_segment(row, study, survey_item_prefix, splitter, df_questionnaire)
+				elif row['QuestionElement'] == 'Answer':
+					df_questionnaire = process_response_segment(row, study, survey_item_prefix, df_questionnaire)
 					
 
 
