85659032e9f37b0942bfa9403d3f8a0d985edffd
dsorato <danielly.sorato@gmail.com>
2021-03-12 17:54:28 +0100

    Changing tag type from pos to upos fixed the problem of flair generating models that predict nothing

--- annotation/pos_tagging.py
+++ annotation/pos_tagging.py
@@ -1,11 +1,9 @@
 from flair.models import SequenceTagger
 import sys
-import pandas as pd
 from flair.data import Sentence
-# from retrieve_from_tables import *
 import flair.datasets
 from flair.trainers import ModelTrainer
-from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings, FlairEmbeddings
+from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings, FlairEmbeddings, FastTextEmbeddings
 
 
 
@@ -36,12 +34,32 @@ def train_cat_tagger():
 
 	trainer.train('/home/danielly/workspace',learning_rate=0.1,mini_batch_size=32, max_epochs=150)
 
-def train_rus_tagger():
-	corpus = flair.datasets.UD_RUSSIAN()
-	tag_type = 'pos'
+
+def train_por_tagger():
+	corpus = flair.datasets.UD_PORTUGUESE()
+	tag_type = 'upos'
 	tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
 
-	embedding_types = [WordEmbeddings('ru')]
+	
+	# embedding_types = [WordEmbeddings('pt')]
+	
+	embedding_types = [FlairEmbeddings('pt-forward'),FlairEmbeddings('pt-backward')]
+
+	embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
+
+	tagger: SequenceTagger = SequenceTagger(hidden_size=256,embeddings=embeddings,tag_dictionary=tag_dictionary,tag_type=tag_type,use_crf=True)
+
+	trainer: ModelTrainer = ModelTrainer(tagger, corpus)
+
+	trainer.train('/home/danielly/workspace',learning_rate=0.1,mini_batch_size=32, max_epochs=150)
+
+def train_rus_tagger():
+	corpus = flair.datasets.UD_RUSSIAN()
+	tag_type = 'upos'
+	tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
+
+
+	embedding_types = [FastTextEmbeddings('/home/danielly/workspace/trained_pos_models/ru/ru.bin')]
 	embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
 
 	tagger: SequenceTagger = SequenceTagger(hidden_size=256,embeddings=embeddings,tag_dictionary=tag_dictionary,tag_type=tag_type,use_crf=True)
@@ -52,9 +70,11 @@ def train_rus_tagger():
 
 def main():
 
-	train_cat_tagger()
+	# train_cat_tagger()
 
-	# train_rus_tagger()
+	# train_por_tagger()
+
+	train_rus_tagger()
 	
 	# languages  = ['POR', 'NOR', 'SPA', 'CAT', 'GER', 'CZE', 'FRE']
 
--- test_pos.py
+++ test_pos.py
@@ -5,23 +5,9 @@ from flair.data import Sentence
 
 
 
-
-def get_pos_model(language):
-	if 'ENG' in language:
-		return SequenceTagger.load('upos') 
-	elif 'CAT' in language:
-		
-		train_tagger(corpus)
-	elif 'RUS' in language:
-		corpus = flair.datasets.UD_RUSSIAN()
-		train_tagger(corpus)
-	else:
-		return SequenceTagger.load('pos-multi') 
-
-
 def main():
 
-	tagger = SequenceTagger.load('/home/danielly/workspace/trained_pos_models/cat_40ep.pt') 
+	tagger = SequenceTagger.load('/home/danielly/workspace/trained_pos_models/cat_150ep.pt') 
 
 	df = pd.read_csv('/home/danielly/workspace/pcsq_data/ESS/R01_2002/spreadsheets/ESS_R01_2002_CAT_ES.csv', sep='\t')
 
